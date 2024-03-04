de90b40276fe32112b2559613577f56f4f250eab
Pratik Rathore <pratikr@stanford.edu>
2024-03-04 15:53:04 -0800

    Added synthetic data generation + testing

---
+++ config/synthetic_testing.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+dataset=synthetic
+data_loc=asdf
+sigma=1.0
+lambd=0.1
+opts=(bcd abcd)
+b=10
+ranks=(10 30 50 100)
+max_iter=30000
+log_freq=100
+seed=0
+devices=(0 1 2 3 4 5 6 7)
+wandb_project=synthetic_testing
+
+# Initialize the counter
+counter=0
+
+# Trap SIGINT (Ctrl-C) and SIGTERM to kill child processes
+trap "kill 0" SIGINT SIGTERM
+
+for opt in "${opts[@]}"
+do
+    for r in "${ranks[@]}"
+    do
+        device=${devices[counter]}
+        python run_experiment.py --dataset $dataset --data_loc $data_loc --sigma $sigma --lambd $lambd --opt $opt --b $b --r $r --max_iter $max_iter \
+                                    --log_freq $log_freq --seed $seed --device $device --wandb_project $wandb_project &
+        counter=$((counter+1))
+        # Ensure we don't exceed the number of devices
+        if [ $counter -eq ${#devices[@]} ]; then
+            counter=0
+        fi
+    done
+done
+
+# Wait for all background processes to finish
+wait
--- src/utils.py
+++ src/utils.py
@@ -18,31 +18,40 @@ def set_random_seed(seed):
 
 # Modify to accomodate other datasets
 def load_data(dataset, data_loc, device):
-    if dataset == 'susy':
+    if dataset == 'synthetic':
+        Xtr = torch.randn(100000, 10, device=device)
+        Xtst = torch.randn(10000, 10, device=device)
+        Xtr.requires_grad = True
+        Xtst.requires_grad = True
+
+        a = torch.randn(10, device=device)
+        ytr = torch.sign(Xtr @ a)
+        ytst = torch.sign(Xtst @ a)
+    elif dataset == 'susy':
         data = load_svmlight_file(data_loc)
+
+        X, y = data[0], data[1]
+        y[y == 0] = -1
+        X = X.toarray()
+
+        Xtr = X[:4500000]
+        Xtst = X[4500000:]
+        ytr = y[:4500000]
+        ytst = y[4500000:]
+
+        scaler = StandardScaler() 
+        scaler.fit(Xtr)
+        Xtr = scaler.transform(Xtr)
+        Xtst = scaler.transform(Xtst)
+
+        Xtr = torch.from_numpy(Xtr).float().to(device)
+        Xtst = torch.from_numpy(Xtst).float().to(device)
+        Xtr.requires_grad = True
+        Xtst.requires_grad = True
+
+        ytr = torch.from_numpy(ytr).float().to(device)
+        ytst = torch.from_numpy(ytst).float().to(device)
     else:
         raise ValueError('We do not support this dataset yet')
-
-    X, y = data[0], data[1]
-    y[y == 0] = -1
-    X = X.toarray()
-
-    Xtr = X[:4500000]
-    Xtst = X[4500000:]
-    ytr = y[:4500000]
-    ytst = y[4500000:]
-
-    scaler = StandardScaler() 
-    scaler.fit(Xtr)
-    Xtr = scaler.transform(Xtr)
-    Xtst = scaler.transform(Xtst)
-
-    Xtr = torch.from_numpy(Xtr).float().to(device)
-    Xtst = torch.from_numpy(Xtst).float().to(device)
-    Xtr.requires_grad = True
-    Xtst.requires_grad = True
-
-    ytr = torch.from_numpy(ytr).float().to(device)
-    ytst = torch.from_numpy(ytst).float().to(device)
 
     return Xtr, Xtst, ytr, ytst
