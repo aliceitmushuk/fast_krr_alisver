78b8d708492272021c52c0ab35a30b9aabf4c1e9
Zachary Frangella <60199061+zjf4@users.noreply.github.com>
2024-04-23 13:54:01 -0700

    Update partial_cholesky
    Removes pykeops, now uses get_row() method instead.

--- src/preconditioners/partial_cholesky.py
+++ src/preconditioners/partial_cholesky.py
@@ -1,6 +1,4 @@
 import torch
-from pykeops.torch import LazyTensor
-from ..kernels.kernel_inits import _get_kernel
 
 
 class PartialCholesky:
@@ -12,25 +10,20 @@ class PartialCholesky:
         self.U = None
         self.S = None
 
-    def update(self, x, kernel_params, diag_K, tol=10**-6):
+    def update(self, x, kernel_params, K,diag_K, tol=10**-6):
         n = x.shape[0]
         L = torch.zeros(self.r, n).to(self.device)
-        x_f = LazyTensor(x[:, None, :])
         diag_K = diag_K.to(self.device)
-
-        def get_row(x):
-            return _get_kernel(x_f, x, kernel_params)
 
         for i in range(self.r):
             # Find pivot index corresponding to maximum diagonal entry
             idx = torch.argmax(diag_K)
-            # Instantiate lazy tensor for datapoint corresponding to this index
-            x_idx = LazyTensor(x[idx])
-            # Lazy tensor for kernelized datapoint
-            k_idx_lz = get_row(x_idx)
-            # Instantiate kernel row as a dense tensor
-            k_idx = k_idx_lz @ torch.ones(1).to(self.device)
-            # Cholesky update
+            
+            # Fetch corresponding datapoint and compute corresponding row of the kernel
+            x_idx = x[idx]
+            k_idx = K.get_row(x_idx, x, kernel_params)
+            
+            # Cholesky factor update
             L[i, :] = (k_idx - torch.matmul(L[:i, idx].t(), L[:i, :])) / torch.sqrt(
                 diag_K[idx]
             )
@@ -39,6 +32,7 @@ class PartialCholesky:
             diag_K = diag_K.clip(min=0)
             if torch.max(diag_K) <= tol:
                 break
+        
         # Return approximation in terms of approximate eigendecomposition
         U, S, _ = torch.linalg.svd(L.T, full_matrices=False)
         self.U = U
