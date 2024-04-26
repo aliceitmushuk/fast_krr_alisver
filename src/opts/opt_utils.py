30441873e528e06ccd32a08d4e43348dcbbd3b85
Pratik Rathore <pratikr@stanford.edu>
2024-04-26 14:25:30 -0700

    Cleaned up code in nystrom preconditioner and opt_utils

--- src/opts/opt_utils.py
+++ src/opts/opt_utils.py
@@ -23,48 +23,6 @@ def _get_L(mat_lin_op, precond_inv_sqrt_lin_op, n, device):
 
     return max_eig
 
-
-# def _get_L(mat_lin_op, precond_inv_lin_op, n, device):
-#     np_type = np.float32 if torch.get_default_dtype() == torch.float32 else np.float64
-
-#     v = torch.randn(n, device=device)
-#     v = v / torch.linalg.norm(v)
-
-#     def P_inv_mat(x):
-#         # Put x on the device as a torch tensor
-#         x = torch.tensor(x, device=device, dtype=torch.get_default_dtype())
-
-#         # Apply the matrix operator
-#         x = mat_lin_op(x)
-
-#         # Apply the preconditioner
-#         x = precond_inv_lin_op(x)
-
-#         # Return the result as a numpy array
-#         return x.cpu().numpy().astype(np_type)
-
-#     def mat_P_inv(x):
-#         # Put x on the device as a torch tensor
-#         x = torch.tensor(x, device=device, dtype=torch.get_default_dtype())
-
-#         # Apply the preconditioner
-#         x = precond_inv_lin_op(x)
-
-#         # Apply the matrix operator
-#         x = mat_lin_op(x)
-
-#         # Return the result as a numpy array
-#         return x.cpu().numpy().astype(np_type)
-
-#     M = LinearOperator((n, n), matvec=P_inv_mat, rmatvec=mat_P_inv)
-#     max_eig = eigs(M, k=1, which="LR", maxiter=100, tol=1e-1, return_eigenvectors=False)[0]
-
-#     print("max_eig: ", max_eig)
-
-#     # Return real part of max_eig as a torch tensor
-#     return torch.tensor(max_eig, device=device).real
-
-
 def _apply_precond(v, precond):
     if precond is not None:
         return precond.inv_lin_op(v)
--- src/preconditioners/nystrom.py
+++ src/preconditioners/nystrom.py
@@ -11,9 +11,6 @@ class Nystrom:
         self.S = None
         self.L = None
 
-        # self.alpha = 0.999 if torch.get_default_dtype() == torch.float32 else 1.0 # Fudge factor to try to ensure the preconditioner is positive definite
-        # self.beta = 1 - (1 - self.alpha) ** 0.5
-
     def update(self, K_lin_op, K_trace, n):
         # Calculate sketch
         Phi = torch.randn((n, self.r), device=self.device) / (n**0.5)
@@ -36,16 +33,11 @@ class Nystrom:
         S = torch.max(torch.square(S) - shift, torch.tensor(0.0))
 
         self.U = U
-        # self.U = torch.linalg.qr(U, mode="reduced")[0] if torch.get_default_dtype() == torch.float32 else U
         self.S = S
 
         # Set indices of self.S that are equal to 0 to a small value
         self.S[self.S < torch.finfo(self.S.dtype).eps] = 1e-4
 
-        # self.M = self.U * torch.sqrt(self.S)[None, :]
-
-        # print("self.S: ", self.S)
-        # print(torch.linalg.norm(self.U.t() @ self.U - torch.eye(self.r, device=self.device), ord=2))
 
     def inv_lin_op(self, v):
         if self.L is None:
@@ -62,35 +54,6 @@ class Nystrom:
         )
         v = (v - self.U @ torch.squeeze(LT_inv_L_inv_UTv, 1)) / self.rho
 
-        # if self.L is None:
-        #     self.L = torch.linalg.cholesky(
-        #         self.rho * torch.diag(self.S**-1) + self.U.T @ self.U
-        #     )
-        #     self.M = torch.linalg.solve_triangular(self.L, self.U.T, upper=False, left=True)
-        #     # Multiply M by -i
-        #     self.M = self.M * -1j
-
-        # complex_type = torch.complex64 if torch.get_default_dtype() == torch.float32 else torch.complex128
-
-        # v = ((v + self.M.t().conj() @ (self.M @ v.to(complex_type)))).real
-        # v /= self.rho
-
-        # if self.L is None:
-        #     self.L = torch.linalg.cholesky(self.rho * torch.eye(self.r, device=self.device) + self.M.t() @ self.M)
-        #     print(torch.linalg.norm(self.L @ self.L.t() - (self.rho * torch.eye(self.r, device=self.device) + self.M.t() @ self.M), ord=2))
-
-        # MTv = self.M.t() @ v
-        # L_inv_MTv = torch.linalg.solve_triangular(
-        #     self.L, torch.unsqueeze(MTv, 1), upper=False, left=True
-        # )
-        # LT_inv_L_inv_MTv = torch.linalg.solve_triangular(
-        #     self.L.t(), L_inv_MTv, upper=True, left=True
-        # )
-        # v = (v - self.M @ torch.squeeze(LT_inv_L_inv_MTv, 1)) / self.rho
-
-        # UTv = self.U.t() @ v
-        # v = v / self.rho - self.alpha * self.U @ (UTv / self.rho - UTv / (self.S + self.rho))
-
         return v
 
     def inv_sqrt_lin_op(self, v):
@@ -98,7 +61,5 @@ class Nystrom:
         v = self.U @ (UTv / ((self.S + self.rho) ** (0.5))) + 1 / (self.rho**0.5) * (
             v - self.U @ UTv
         )
-        # v = 1 / (self.rho**0.5) * v - self.U @ (self.beta / (self.rho ** 0.5)
-        #                                         * UTv - (self.alpha ** 0.5) / ((self.S + self.rho) ** (0.5)) * UTv)
 
         return v
