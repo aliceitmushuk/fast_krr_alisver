a8db2a1a59741120008b63ffdb05debbec49ec09
Pratik Rathore <pratikr@stanford.edu>
2024-04-26 14:23:34 -0700

    Tried new things out with Nystrom approximation + changed _get_L calculation back to what it was before

--- src/opts/opt_utils.py
+++ src/opts/opt_utils.py
@@ -1,61 +1,68 @@
 import torch
-from scipy.sparse.linalg import LinearOperator, eigs
+# import numpy as np
+# from scipy.sparse.linalg import LinearOperator, eigs
 
-# def _get_L(mat_lin_op, precond_inv_sqrt_lin_op, n, device):
-#     v = torch.randn(n, device=device)
-#     v = v / torch.linalg.norm(v)
-
-#     max_eig = None
-
-#     for _ in range(10):  # TODO: Make this a parameter or check tolerance instead
-#         v_old = v.clone()
-
-#         v = precond_inv_sqrt_lin_op(v)
-#         v = mat_lin_op(v)
-#         v = precond_inv_sqrt_lin_op(v)
-
-#         max_eig = torch.dot(v_old, v)
-
-#         v = v / torch.linalg.norm(v)
-
-#     return max_eig
-
-
-def _get_L(mat_lin_op, precond_inv_lin_op, n, device):
+def _get_L(mat_lin_op, precond_inv_sqrt_lin_op, n, device):
     v = torch.randn(n, device=device)
     v = v / torch.linalg.norm(v)
 
-    def P_inv_mat(x):
-        # Put x on the device as a torch tensor
-        x = torch.tensor(x, device=device, dtype=torch.get_default_dtype())
+    max_eig = None
 
-        # Apply the matrix operator
-        x = mat_lin_op(x)
+    for _ in range(10):  # TODO: Make this a parameter or check tolerance instead
+        v_old = v.clone()
 
-        # Apply the preconditioner
-        x = precond_inv_lin_op(x)
+        v = precond_inv_sqrt_lin_op(v)
+        v = mat_lin_op(v)
+        v = precond_inv_sqrt_lin_op(v)
 
-        # Return the result as a numpy array
-        return x.cpu().numpy()
+        max_eig = torch.dot(v_old, v)
 
-    def mat_P_inv(x):
-        # Put x on the device as a torch tensor
-        x = torch.tensor(x, device=device, dtype=torch.get_default_dtype())
+        v = v / torch.linalg.norm(v)
 
-        # Apply the preconditioner
-        x = precond_inv_lin_op(x)
+    print("max_eig: ", max_eig)
 
-        # Apply the matrix operator
-        x = mat_lin_op(x)
+    return max_eig
 
-        # Return the result as a numpy array
-        return x.cpu().numpy()
 
-    M = LinearOperator((n, n), matvec=P_inv_mat, rmatvec=mat_P_inv)
-    max_eig = eigs(M, k=1, which="LR", return_eigenvectors=False)[0]
+# def _get_L(mat_lin_op, precond_inv_lin_op, n, device):
+#     np_type = np.float32 if torch.get_default_dtype() == torch.float32 else np.float64
 
-    # Return real part of max_eig as a torch tensor
-    return torch.tensor(max_eig, device=device).real
+#     v = torch.randn(n, device=device)
+#     v = v / torch.linalg.norm(v)
+
+#     def P_inv_mat(x):
+#         # Put x on the device as a torch tensor
+#         x = torch.tensor(x, device=device, dtype=torch.get_default_dtype())
+
+#         # Apply the matrix operator
+#         x = mat_lin_op(x)
+
+#         # Apply the preconditioner
+#         x = precond_inv_lin_op(x)
+
+#         # Return the result as a numpy array
+#         return x.cpu().numpy().astype(np_type)
+
+#     def mat_P_inv(x):
+#         # Put x on the device as a torch tensor
+#         x = torch.tensor(x, device=device, dtype=torch.get_default_dtype())
+
+#         # Apply the preconditioner
+#         x = precond_inv_lin_op(x)
+
+#         # Apply the matrix operator
+#         x = mat_lin_op(x)
+
+#         # Return the result as a numpy array
+#         return x.cpu().numpy().astype(np_type)
+
+#     M = LinearOperator((n, n), matvec=P_inv_mat, rmatvec=mat_P_inv)
+#     max_eig = eigs(M, k=1, which="LR", maxiter=100, tol=1e-1, return_eigenvectors=False)[0]
+
+#     print("max_eig: ", max_eig)
+
+#     # Return real part of max_eig as a torch tensor
+#     return torch.tensor(max_eig, device=device).real
 
 
 def _apply_precond(v, precond):
