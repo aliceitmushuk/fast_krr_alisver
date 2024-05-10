0577a04bcf610337eb43ef34080a8b0e2af912e1
Pratik Rathore <pratikr@stanford.edu>
2024-05-09 18:03:55 -0700

    Fixed bug in nystrom approx + updated sksaga config for taxi

--- src/preconditioners/nystrom.py
+++ src/preconditioners/nystrom.py
@@ -33,13 +33,24 @@ class Nystrom:
 
         # Will either be on self.device or "cpu" depending on self.use_cpu
         C = torch.linalg.cholesky(cholesky_target)
-        B = torch.linalg.solve_triangular(
-            C.t(), Y_shifted, upper=True, left=False)
+
+        try:
+            B = torch.linalg.solve_triangular(
+                C.t(), Y_shifted, upper=True, left=False)
+        # temporary fix for issue @ https://github.com/pytorch/pytorch/issues/97211
+        except RuntimeError as e:
+            if "CUBLAS_STATUS_EXECUTION_FAILED" in str(e):
+                torch.cuda.empty_cache()
+                B = torch.linalg.solve_triangular(C.t().to("cpu"), Y_shifted.to(
+                    "cpu"), upper=True, left=False).to(self.device)
+            else:
+                raise e
 
         del cholesky_target, Y_shifted, C, Phi
         torch.cuda.empty_cache()
 
         U, S, _ = torch.linalg.svd(B, full_matrices=False)
+        S = torch.max(torch.square(S) - shift, torch.tensor(0.0))
         if self.use_cpu:
             U, S = U.to(self.device), S.to(self.device)
 
@@ -113,84 +124,3 @@ class Nystrom:
         )
 
         return v
-
-
-
-# import torch
-
-
-# class Nystrom:
-#     def __init__(self, device, r, rho=None):
-#         self.device = device
-#         self.r = r
-#         self.rho = rho
-
-#         self.U = None
-#         self.S = None
-#         self.L = None
-
-#     def update(self, K_lin_op, K_trace, n):
-#         # Calculate sketch
-#         Phi = torch.randn((n, self.r), device=self.device) / (n**0.5)
-#         try:
-#             Phi = torch.linalg.qr(Phi, mode="reduced")[0]
-#         except RuntimeError:  # Perform calculations on CPU and transfer back if we run out of memory
-#             Phi = torch.linalg.qr(Phi.to('cpu'), mode="reduced")[0].to(self.device)
-#         Phi = Phi.contiguous()  # Ensure memory is contiguous to prevent pyKeOps warning
-
-#         Y = K_lin_op(Phi)
-
-#         # Calculate shift
-#         shift = torch.finfo(Y.dtype).eps * K_trace
-#         Y_shifted = Y + shift * Phi
-
-#         # Calculate Phi^T * K * Phi (w/ shift) for Cholesky
-#         choleskytarget = torch.mm(Phi.t(), Y_shifted)
-
-#         # Perform Cholesky decomposition
-#         C = torch.linalg.cholesky(choleskytarget)
-
-#         try:
-#             B = torch.linalg.solve_triangular(C.t(), Y_shifted, upper=True, left=False)
-#         # temporary fix for issue @ https://github.com/pytorch/pytorch/issues/97211
-#         except:
-#             B = torch.linalg.solve_triangular(C.t().to('cpu'), Y_shifted.to(
-#                 'cpu'), upper=True, left=False).to(self.device)
-
-#         try:
-#             U, S, _ = torch.linalg.svd(B, full_matrices=False)
-#         except RuntimeError: # Perform calculations on CPU and transfer back if we run out of memory
-#             U, S, _ = torch.linalg.svd(B.to('cpu'), full_matrices=False)
-#             U, S = U.to(self.device), S.to(self.device)
-#         S = torch.max(torch.square(S) - shift, torch.tensor(0.0))
-
-#         self.U = U
-#         self.S = S
-
-#         # Set indices of self.S that are equal to 0 to a small value
-#         self.S[self.S < torch.finfo(self.S.dtype).eps] = 1e-4
-
-#     def inv_lin_op(self, v):
-#         if self.L is None:
-#             self.L = torch.linalg.cholesky(
-#                 self.rho * torch.diag(self.S**-1) + self.U.T @ self.U
-#             )
-
-#         UTv = self.U.t() @ v
-#         L_inv_UTv = torch.linalg.solve_triangular(
-#             self.L, torch.unsqueeze(UTv, 1), upper=False, left=True
-#         )
-#         LT_inv_L_inv_UTv = torch.linalg.solve_triangular(
-#             self.L.t(), L_inv_UTv, upper=True, left=True
-#         )
-#         v = (v - self.U @ torch.squeeze(LT_inv_L_inv_UTv, 1)) / self.rho
-
-#         return v
-
-#     def inv_sqrt_lin_op(self, v):
-#         UTv = self.U.t() @ v
-#         v = self.U @ (UTv / ((self.S + self.rho) ** (0.5))) + 1 / (self.rho**0.5) * (
-#             v - self.U @ UTv
-#         )
-
-#         return v
