b88d7a36f3dc88444f1496c4907d45922d34b1a9
Pratik Rathore <pratikr@stanford.edu>
2024-11-11 14:56:56 -0800

    made methods in kernels static

--- src/kernels/rbf.py
+++ src/kernels/rbf.py
@@ -4,15 +4,13 @@ from .kernel import Kernel
 
 
 class Rbf(Kernel):
-    def __init__(self, x1_lazy, x2_lazy, kernel_params):
-        super().__init__(x1_lazy, x2_lazy, kernel_params)
-
     @staticmethod
     def _check_kernel_params(kernel_params):
         if "sigma" not in kernel_params:
             raise ValueError("Missing sigma for RBF kernel")
 
-    def _compute_kernel(self, x1_lazy, x2_lazy, kernel_params):
+    @staticmethod
+    def _compute_kernel(x1_lazy, x2_lazy, kernel_params):
         Rbf._check_kernel_params(kernel_params)
 
         D = ((x1_lazy - x2_lazy) ** 2).sum(dim=2)
@@ -20,14 +18,15 @@ class Rbf(Kernel):
 
         return K
 
-    def get_diag(self):
-        if self.K.shape[0] != self.K.shape[1]:
-            raise ValueError("The kernel matrix is not square")
-        return torch.ones(self.K.shape[0])
-
-    def get_trace(self):
-        return self.get_diag().sum().item()
-
-    def get_row(self, x_i, x, kernel_params):
+    @staticmethod
+    def _get_row(x_i, x, kernel_params):
         D = ((x_i - x) ** 2).sum(dim=1)
         return (-D / (2 * kernel_params["sigma"] ** 2)).exp()
+
+    @staticmethod
+    def _get_diag(n):
+        return torch.ones(n)
+
+    @staticmethod
+    def _get_trace(n):
+        return Rbf._get_diag(n).sum().item()
