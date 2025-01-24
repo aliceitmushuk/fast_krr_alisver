c9ee9c616dd3d30ef720ba898fff2d99d7e12d03
Pratik Rathore <pratikr@stanford.edu>
2025-01-24 14:58:34 -0800

    add histograms for ratio of per-iteration times

--- plotting/base_utils.py
+++ plotting/base_utils.py
@@ -65,7 +65,7 @@ def filter_runs_union(runs, criteria_list):
     return runs_filtered
 
 
-def get_datapasses(run, steps):
+def _get_datapasses(run, steps):
     opt = run.config["opt"]
     n = run.config["n"]
     m = run.config.get("m", None)
@@ -86,14 +86,24 @@ def get_datapasses(run, steps):
     return scaling_factor * steps
 
 
+def _get_cum_times(run):
+    times_hist = run.scan_history(keys=["iter_time"])
+    times = np.array([time["iter_time"] for time in times_hist])
+    cum_times = np.cumsum(times)
+    return cum_times
+
+
+def _get_avg_time(run):
+    cum_times = _get_cum_times(run)
+    return cum_times[-1] / cum_times.shape[0]
+
+
 def get_x(run, steps, x_axis):
     if x_axis == "time":
-        times_hist = run.scan_history(keys=["iter_time"])
-        times = np.array([time["iter_time"] for time in times_hist])
-        cum_times = np.cumsum(times)
+        cum_times = _get_cum_times(run)
         return cum_times[steps]
     elif x_axis == "datapasses":
-        return get_datapasses(run, steps)
+        return _get_datapasses(run, steps)
     elif x_axis == "iters":
         return steps
 
@@ -312,6 +322,41 @@ def plot_runs_grid(
         unique_labels.keys(),
         **LEGEND_SPECS,
     )
+    plt.tight_layout()
+
+    if save_path is not None:
+        plt.savefig(save_path, bbox_inches="tight")
+
+    plt.close(fig)
+
+
+def plot_per_iter_runtime_ratios(
+    run_pairs,
+    x_label,
+    save_dir=None,
+    save_name=None,
+):
+    save_path = get_save_path(save_dir, save_name)
+    avg_time_ratios = []
+
+    fig, ax = plt.subplots(figsize=(SZ_COL, SZ_ROW))
+    for run_pair in run_pairs:
+        # Plot the minimum average time for each pair of runs
+        min_avg_time_x = np.inf
+        min_avg_time_y = np.inf
+        for run in run_pair["x"]:
+            avg_time = _get_avg_time(run)
+            min_avg_time_x = min(min_avg_time_x, avg_time)
+        for run in run_pair["y"]:
+            avg_time = _get_avg_time(run)
+            min_avg_time_y = min(min_avg_time_y, avg_time)
+        time_ratio = min_avg_time_x / min_avg_time_y
+        # Check that time ratio is not infinite
+        if not np.isinf(time_ratio):
+            avg_time_ratios.append(time_ratio)
+
+    ax.hist(avg_time_ratios, bins=20, color="blue", alpha=1.0)
+    ax.set_xlabel(x_label)
     plt.tight_layout()
 
     if save_path is not None:
---
+++ plotting/iter_times.py
@@ -0,0 +1,76 @@
+from tqdm import tqdm
+
+from constants import (
+    USE_LATEX,
+    FONTSIZE,
+    BASE_SAVE_DIR,
+    EXTENSION,
+)
+
+from constants import ENTITY_NAME, PROJECT_FULL_KRR
+from constants import PERFORMANCE_DATASETS_CFG
+from base_utils import set_fontsize, render_in_latex, plot_per_iter_runtime_ratios
+from cfg_utils import get_save_dir, create_krr_config, _get_filtered_runs
+
+# save directory
+SAVE_DIR = "iter_times"
+
+X_LABEL = "Per-iteration speedup"
+
+# filters for runs
+ASKOTCH_FILTER = {
+    "optimizer": lambda run: run.config["opt"] == "askotchv2",
+    "accelerated": lambda run: run.config["accelerated"],
+    "preconditioned": lambda run: run.config["precond_params"] is not None,
+    "rho_damped": lambda run: run.config.get("precond_params", {}).get("rho", None)
+    == "damped",
+    "sampling": lambda run: run.config["sampling_method"] == "uniform",
+    "finished": lambda run: run.state == "finished",
+}
+PCG_FLOAT32_FILTER = {
+    "optimizer": lambda run: run.config["opt"] == "pcg",
+    "precision": lambda run: run.config["precision"] == "float32",
+    "finished": lambda run: run.state == "finished",
+}
+PCG_FLOAT64_FILTER = {
+    "optimizer": lambda run: run.config["opt"] == "pcg",
+    "precision": lambda run: run.config["precision"] == "float64",
+    "finished": lambda run: run.state == "finished",
+}
+
+if __name__ == "__main__":
+    set_fontsize(FONTSIZE)
+    if USE_LATEX:
+        render_in_latex()
+
+    run_pairs_float32 = []
+    run_pairs_float64 = []
+
+    askotch_cfg_float32 = create_krr_config(PROJECT_FULL_KRR, [ASKOTCH_FILTER])
+    pcg_cfg_float32 = create_krr_config(PROJECT_FULL_KRR, [PCG_FLOAT32_FILTER])
+    pcg_cfg_float64 = create_krr_config(PROJECT_FULL_KRR, [PCG_FLOAT64_FILTER])
+
+    # Loop through all configs in the datasets config and extract the dataset name
+    for ds_config in PERFORMANCE_DATASETS_CFG:
+        for ds in ds_config["datasets"].keys():
+            runs_pcg_float32 = _get_filtered_runs(pcg_cfg_float32, ds, ENTITY_NAME)
+            runs_pcg_float64 = _get_filtered_runs(pcg_cfg_float64, ds, ENTITY_NAME)
+            runs_askotch = _get_filtered_runs(askotch_cfg_float32, ds, ENTITY_NAME)
+            run_pairs_float32.append({"x": runs_pcg_float32, "y": runs_askotch})
+            run_pairs_float64.append({"x": runs_pcg_float64, "y": runs_askotch})
+
+    with tqdm(total=2, desc="Iteration times") as pbar:
+        plot_per_iter_runtime_ratios(
+            run_pairs_float32,
+            x_label=X_LABEL,
+            save_dir=get_save_dir(BASE_SAVE_DIR, SAVE_DIR),
+            save_name=f"float32.{EXTENSION}",
+        )
+        pbar.update(1)
+        plot_per_iter_runtime_ratios(
+            run_pairs_float64,
+            x_label=X_LABEL,
+            save_dir=get_save_dir(BASE_SAVE_DIR, SAVE_DIR),
+            save_name=f"float64.{EXTENSION}",
+        )
+        pbar.update(1)
