7ebc099e01494c7512365cca92ad846135c4b09e
Pratik Rathore <pratikr@stanford.edu>
2025-01-28 12:21:40 -0800

    add eigenpro3

--- src/opts/eigenpro3.py
+++ src/opts/eigenpro3.py
@@ -15,11 +15,9 @@ class EigenPro4(EigenPro):
         bg: int,
         block_sz: int,
         r: int,
-        proj_freq: int,
         proj_inner_iters: Optional[int] = 10,
     ):
         super().__init__(model, bg, block_sz, r)
-        self.proj_freq = proj_freq
         self.proj_inner_iters = proj_inner_iters
         self._apply_precond, self.eta, self.block = self._setup()
         self.Kzb = self.K_fn(
@@ -27,7 +25,7 @@ class EigenPro4(EigenPro):
             self.model.x[self.block],
             get_row=False,
         )
-        self._reset()
+        self.h = torch.zeros_like(self.model.w, device=self.model.device)
 
     def _setup(self):
         eigvals, eigvecs, beta, tail_eigval, block = self._get_top_eigensys()
@@ -39,13 +37,6 @@ class EigenPro4(EigenPro):
         eta = self._compute_eta(tail_eigval, beta)
 
         return _apply_precond, eta, block
-
-    def _reset(self):
-        self.Z_tmp = torch.empty(0, device=self.model.device, dtype=torch.int64)
-        self.alpha_tmp = torch.empty(0, device=self.model.device)
-        self.alpha_b = torch.zeros(self.block_sz, device=self.model.device)
-        self.h = torch.zeros_like(self.model.w, device=self.model.device)
-        self.n_inner_iters = 0
 
     def _project(self, v):
         proj_model = FullKRR(
@@ -76,24 +67,6 @@ class EigenPro4(EigenPro):
             self.model.x[idx], self.model.x[self.model.inducing_pts], get_row=False
         )
         grad = Kmz @ self.model.w - self.model.b[idx]
-        if self.Z_tmp.shape[0] > 0:
-            Kmz_temp = self.K_fn(
-                self.model.x[idx], self.model.x[self.Z_tmp], get_row=False
-            )
-            grad += Kmz_temp @ self.alpha_tmp
         Kbm = self.K_fn(self.model.x[self.block], self.model.x[idx], get_row=False)
-        grad += Kbm.T @ self.alpha_b
-
-        idx = idx.to(self.model.device)  # to prevent device mismatch
-        self.Z_tmp = torch.cat([self.Z_tmp, idx])
-        self.alpha_tmp = torch.cat([self.alpha_tmp, -self.eta * grad])
-
-        self.alpha_b += self.eta * self._apply_precond(grad, Kbm)
-        self.h -= self.eta * Kmz.T @ grad
-        self.h += self.eta * self.Kzb @ self._apply_precond(grad, Kbm)
-
-        self.n_inner_iters += 1
-        if self.n_inner_iters == self.proj_freq:
-            theta = self._project(self.h)
-            self.model.w -= self.model.n / self.bg * self.eta * theta
-            self._reset()
+        self.h = Kmz.T @ grad - self.Kzb @ self._apply_precond(grad, Kbm)
+        self.model.w -= self.model.n / self.bg * self.eta * self._project(self.h)
