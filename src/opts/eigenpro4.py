dbd336a095808af100083851818995d3be85a171
Pratik Rathore <pratikr@stanford.edu>
2025-01-27 22:24:08 -0800

    finished first implementation of eigenpro4

--- src/opts/eigenpro4.py
+++ src/opts/eigenpro4.py
@@ -1,6 +1,9 @@
+from typing import Optional
+
 import torch
 
-from .optimizer import EigenPro
+from .optimizer import EigenPro, EigenPro2
+from ..models import FullKRR
 from .utils.sgd import _get_minibatch
 
 
@@ -12,9 +15,11 @@ class EigenPro4(EigenPro):
         block_sz: int,
         r: int,
         proj_freq: int,
+        proj_inner_iters: Optional[int] = 10,
     ):
         super().__init__(model, bg, block_sz, r)
         self.proj_freq = proj_freq
+        self.proj_inner_iters = proj_inner_iters
         self._apply_precond, self.eta, self.block = self._setup()
         self.Kzb = self.K_fn(self.model.inducing_pts, self.block, get_row=False)
         self._reset()
@@ -38,7 +43,27 @@ class EigenPro4(EigenPro):
         self.n_inner_iters = 0
 
     def _project(self):
-        pass
+        proj_model = FullKRR(
+            x=self.model.x[self.model.inducing_pts],
+            b=self.h,
+            x_tst=None,
+            b_tst=None,
+            kernel_params=self.model.kernel_params,
+            Ktr_needed=True,
+            lambd=0.0,
+            task="regression",
+            w0=torch.zeros_like(self.model.w, device=self.model.device),
+            device=self.model.device,
+        )
+        eigenpro2_opt = EigenPro2(
+            proj_model,
+            bg=proj_model.n // self.proj_inner_iters,
+            block_sz=min(proj_model.n, 12000),
+            r=100,
+        )
+        for _ in range(self.proj_inner_iters):
+            eigenpro2_opt.step()
+        return eigenpro2_opt.model.w
 
     def step(self):
         idx = _get_minibatch(self.generator)
