1e1fddf091a4467eab8cf5761abce3b82bc83ab5
Pratik Rathore <pratikr@stanford.edu>
2025-02-18 16:58:41 -0800

    remove mimosa from config generation

--- generate_configs_taxi.py
+++ generate_configs_taxi.py
@@ -11,7 +11,6 @@ from generate_configs_full_krr import generate_combinations as gc_full_krr
 from generate_configs_eigenpro2 import generate_combinations as gc_eigenpro2
 from generate_configs_eigenpro3 import generate_combinations as gc_eigenpro3
 from generate_configs_falkon import generate_combinations as gc_falkon
-from generate_configs_mimosa import generate_combinations as gc_mimosa
 
 SEED = 0
 
@@ -19,9 +18,7 @@ PRECONDITIONERS = ["nystrom"]
 CHOLESKY_MODES = [None]
 SAMPLING_MODES = ["uniform"]
 ACC_MODES = [True]
-BG_MODES = [65536]
 RHO_MODES = ["damped", "regularization"]
-RHO_MODES_MIMOSA = [1e6, 3e6, 1e7, 3e7, 1e8]
 BLK_SZ_FRAC = 0.0005
 
 if __name__ == "__main__":
@@ -79,19 +76,6 @@ if __name__ == "__main__":
         "training.max_iter": [None],
         "wandb.project": ["performance_inducing_krr"],
     }
-    sweep_params_mimosa = {
-        "dataset": ["taxi"],
-        "model": ["inducing_krr"],
-        "m": [1_000_000],
-        "opt.type": ["mimosa"],
-        "precond.r": [500],
-        "precond.use_cpu": [False],
-        "training.log_freq": [200],
-        "training.precision": ["float64"],
-        "training.seed": [SEED],
-        "training.max_iter": [None],
-        "wandb.project": ["performance_inducing_krr"],
-    }
 
     combinations_askotchv2 = gc_full_krr(
         sweep_params_askotchv2,
@@ -143,19 +127,8 @@ if __name__ == "__main__":
         PERFORMANCE_TIME_CONFIGS,
         LOG_TEST_ONLY,
     )
-    combinations_mimosa = gc_mimosa(
-        sweep_params_mimosa,
-        KERNEL_CONFIGS,
-        DATA_CONFIGS,
-        LAMBDA_CONFIGS,
-        PERFORMANCE_TIME_CONFIGS,
-        LOG_TEST_ONLY,
-        RHO_MODES_MIMOSA,
-        BG_MODES,
-    )
 
     save_configs(combinations_askotchv2 + combinations_pcg, "taxi_full_krr")
     save_configs(combinations_eigenpro2, "taxi_eigenpro2")
     save_configs(combinations_eigenpro3, "taxi_eigenpro3")
     save_configs(combinations_falkon, "taxi_falkon")
-    save_configs(combinations_mimosa, "taxi_mimosa")
