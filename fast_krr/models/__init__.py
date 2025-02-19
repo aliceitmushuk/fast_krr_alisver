e66a01da849cfa65a046cb5c14e2544dc4861bc6
Pratik Rathore <pratikr@stanford.edu>
2025-02-19 12:35:56 -0800

    update inits within package

--- fast_krr/models/__init__.py
+++ fast_krr/models/__init__.py
@@ -1,4 +1,4 @@
-from .full_krr import FullKRR
-from .inducing_krr import InducingKRR
+from fast_krr.models.full_krr import FullKRR
+from fast_krr.models.inducing_krr import InducingKRR
 
 __all__ = ["FullKRR", "InducingKRR"]
