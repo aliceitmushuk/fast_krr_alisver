aef0375ba819161f3f45adc21c10ecbe8deb5426
aliceitmushuk <110152189+aliceitmushuk@users.noreply.github.com>
2025-07-07 08:04:50 -0700

    Update rbf_invquad.py

--- fast_krr/kernels/rbf_invquad.py
+++ fast_krr/kernels/rbf_invquad.py
@@ -6,22 +6,22 @@ from fast_krr.kernels.kernel import Kernel
 class Rbf_invquad(Kernel):
     @staticmethod
     def _check_kernel_params(kernel_params):
-        if "sigma" not in kernel_params:
-            raise ValueError("Missing sigma for RBF kernel")
+        if "gamma" not in kernel_params:
+            raise ValueError("Missing gamma for RBF invquad kernel")
 
     @staticmethod
     def _get_kernel(x1_lazy, x2_lazy, kernel_params):
         Rbf._check_kernel_params(kernel_params)
 
         D = ((x1_lazy - x2_lazy) ** 2).sum(dim=2)
-        K = 1/((1+(D*kernel_params["sigma"])**2).sqrt())
+        K = 1/((1+(D*kernel_params["gamma"])**2).sqrt())
 
         return K
 
     @staticmethod
     def _get_row(x_i, x, kernel_params):
         D = ((x_i - x) ** 2).sum(dim=1)
-        return 1/((1+(D*kernel_params["sigma"])**2).sqrt())
+        return 1/((1+(D*kernel_params["gamma"])**2).sqrt())
 
     @staticmethod
     def _get_diag(n):
