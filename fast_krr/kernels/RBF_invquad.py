1b3d83fb4013f01df7816d3128219242ac8ca249
aliceitmushuk <110152189+aliceitmushuk@users.noreply.github.com>
2025-07-06 22:17:47 -0700

    Create RBF_invquad.py

---
+++ fast_krr/kernels/RBF_invquad.py
@@ -0,0 +1,32 @@
+import torch
+
+from fast_krr.kernels.kernel import Kernel
+
+
+class Rbf(Kernel):
+    @staticmethod
+    def _check_kernel_params(kernel_params):
+        if "sigma" not in kernel_params:
+            raise ValueError("Missing sigma for RBF kernel")
+
+    @staticmethod
+    def _get_kernel(x1_lazy, x2_lazy, kernel_params):
+        Rbf._check_kernel_params(kernel_params)
+
+        D = ((x1_lazy - x2_lazy) ** 2).sum(dim=2)
+        K = (-D / (2 * kernel_params["sigma"] ** 2)).exp()
+
+        return K
+
+    @staticmethod
+    def _get_row(x_i, x, kernel_params):
+        D = ((x_i - x) ** 2).sum(dim=1)
+        return (-D / (2 * kernel_params["sigma"] ** 2)).exp()
+
+    @staticmethod
+    def _get_diag(n):
+        return torch.ones(n)
+
+    @staticmethod
+    def _get_trace(n):
+        return Rbf._get_diag(n).sum().item()
