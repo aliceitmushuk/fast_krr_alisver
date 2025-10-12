81128d06a3456dfe28c4e924e4a9c37d5223a29e
aliceitmushuk <110152189+aliceitmushuk@users.noreply.github.com>
2025-10-11 23:26:48 -0700

    Update __init__.py

--- fast_krr/opts/__init__.py
+++ fast_krr/opts/__init__.py
@@ -1,5 +1,6 @@
 from fast_krr.opts.askotch import ASkotch
 from fast_krr.opts.askotchv2 import ASkotchV2
+from fast_krr.opts.askotchv3 import ASkotchV3
 from fast_krr.opts.eigenpro2 import EigenPro2
 from fast_krr.opts.eigenpro3 import EigenPro3
 from fast_krr.opts.eigenpro4 import EigenPro4
@@ -9,6 +10,7 @@ from fast_krr.opts.pcg import PCG
 __all__ = [
     "ASkotch",
     "ASkotchV2",
+    "ASkotchV3",
     "EigenPro2",
     "EigenPro3",
     "EigenPro4",
