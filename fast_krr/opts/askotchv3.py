961a9c1cf6af791052d278c9e22d3986323d2857
aliceitmushuk <110152189+aliceitmushuk@users.noreply.github.com>
2025-10-11 23:31:43 -0700

    Update askotchv3.py

--- fast_krr/opts/askotchv3.py
+++ fast_krr/opts/askotchv3.py
@@ -2,6 +2,7 @@ import torch
 
 from fast_krr.opts.optimizer import Optimizer
 from fast_krr.opts.utils.general import _get_leverage_scores
+from fast_krr.opts.utils.general import _get_L, _apply_precond
 from fast_krr.opts.utils.bcd import (
     _get_block,
     _get_block_properties,
