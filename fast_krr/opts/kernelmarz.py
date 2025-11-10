acb13f6ce988cca42602e4c1c0189bbe6a1b60f3
aliceitmushuk <110152189+aliceitmushuk@users.noreply.github.com>
2025-11-10 15:37:05 -0800

    Update kernelmarz.py

--- fast_krr/opts/kernelmarz.py
+++ fast_krr/opts/kernelmarz.py
@@ -85,7 +85,7 @@ class KernelMarz(Optimizer):
         if random.random()<prob_add:
             block = _get_block(self.probs, self.probs_cpu, self.block_sz)
             xb_i = LazyTensor(self.model.x[block][:, None, :])
-            Kbn = _get_kernel(xb_i, self.model.x_j, self.model.kernel_params).to_dense()
+            Kbn = _get_kernel(xb_i, self.model.x_j, self.model.kernel_params)
             Kbn_Kbn_T=keops_matmul(Kbn,Kbn.t(),self.block_sz,self.block_sz)
             L=torch.cholesky(Kbn_Kbn_T+proj_reg*torch.eye(self.block_sz))
             self.cache_chol.append(L)
