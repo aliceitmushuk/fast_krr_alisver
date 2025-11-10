93ffd3aea1132d2abe96e6e1a8f6523c45f03fe5
aliceitmushuk <110152189+aliceitmushuk@users.noreply.github.com>
2025-11-10 12:36:11 -0800

    Update kernelmarz.py

--- fast_krr/opts/kernelmarz.py
+++ fast_krr/opts/kernelmarz.py
@@ -9,7 +9,12 @@ from fast_krr.opts.utils.bcd import (
 from fast_krr.kernels.kernel_inits import (
     _get_kernel,
 )
-
+def keops_matmul(lazy_A,lazy_B):
+    A_aug=lazy_A[:,:,None]
+    B_aug=lazy_B[None,:,:]
+    AB=(A*B).sum(dim=1)
+    return AB
+    
 #also returns a sum of the squared error
 def _get_block_update_w_err_kaczmarz(model, w, L, block):
 
