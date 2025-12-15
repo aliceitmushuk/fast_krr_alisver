94cda5b33eb9575ceecedc23c4d07661b78a99e2
aliceitmushuk <110152189+aliceitmushuk@users.noreply.github.com>
2025-12-15 09:58:18 -0800

    Update askotchv3.py

--- fast_krr/opts/askotchv3.py
+++ fast_krr/opts/askotchv3.py
@@ -97,7 +97,7 @@ class ASkotchV3(Optimizer):
                         self.ratio = self.dist_new / self.dist_old
                     else:
                         self.ratio = self.ratio*(a_old/a_new) + self.dist_new / self.dist_old * (1 - a_old/a_new)
-                    if ratio<=1:
+                    if self.ratio<=1:
                         self.rho = max(self.rho_min,1 - self.ratio**(1/self.p))
                     else:
                         self.rho = min(self.rho*10,1)
