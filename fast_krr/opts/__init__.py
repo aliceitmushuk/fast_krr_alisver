08062a0ae568f5930ca1f5dcfcb1c2f0a69947d9
aliceitmushuk <110152189+aliceitmushuk@users.noreply.github.com>
2025-12-28 00:22:45 -0800

    Update __init__.py

--- fast_krr/opts/__init__.py
+++ fast_krr/opts/__init__.py
@@ -4,7 +4,6 @@ from fast_krr.opts.askotchv3 import ASkotchV3
 from fast_krr.opts.eigenpro2 import EigenPro2
 from fast_krr.opts.eigenpro3 import EigenPro3
 from fast_krr.opts.eigenpro4 import EigenPro4
-from fast_krr.opts.kernelmarz import KernelMarz
 from fast_krr.opts.mimosa import Mimosa
 from fast_krr.opts.pcg import PCG
 
@@ -15,7 +14,6 @@ __all__ = [
     "EigenPro2",
     "EigenPro3",
     "EigenPro4",
-    "KernelMarz",
     "Mimosa",
     "PCG",
 ]
